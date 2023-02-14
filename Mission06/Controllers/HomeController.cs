f2a11b398a135e120fe3ee249f78885c24957735
Austin Cox <ac764@byu.edu>
2023-02-13 23:57:26 -0700

    Add project files.

---
+++ Mission06/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private DateApplicationContext blahContext { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, DateApplicationContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Movie ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult datingapplication ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult datingapplication(ApplicationResponse ar)
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges();
+            return View("Confirmation", ar);
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06/Models/DateApplicationContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06.Models
+{
+    public class DateApplicationContext : DbContext
+    {
+        //Constructor
+        public DateApplicationContext (DbContextOptions<DateApplicationContext> options) : base(options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<ApplicationResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            _ = mb.Entity<ApplicationResponse>().HasData(
+
+                new ApplicationResponse
+                {
+                    ApplicationID = 1,
+                    FirstName = "Michael",
+                    LastName = "Scott",
+                    Age = 43,
+                    Phone = "8018018018",
+                    Major = "Paper Stuff",
+                    CreeperStalker = false
+                },
+                new ApplicationResponse
+                {
+                    ApplicationID = 2,
+                    FirstName = "Creed",
+                    LastName = "Bratton",
+                    Age = 90,
+                    Phone = "803iu2ho2",
+                    Major = "Undeclared",
+                    CreeperStalker = true
+                }
+                );
+        }
+    }
+}
